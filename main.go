11077ef1528f0ad97f5190acd7d06dc09873d1ee
motomux <motomux@gmail.com>
2016-09-10 15:49:47 -0700

    Connect to tarantool

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	tarantool "github.com/tarantool/go-tarantool"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +14,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	server := "smart-cooking-db:3301"
+	opts := tarantool.Opts{
+		Timeout:       500 * time.Millisecond,
+		Reconnect:     1 * time.Second,
+		MaxReconnects: 3,
+	}
+
+	client, err := tarantool.Connect(server, opts)
+	if err != nil {
+		log.Fatalf("Failed to connect: %s", err.Error())
+	}
+	log.Println("Connected to tarantool")
+
 	http.HandleFunc("/", handler)
 	log.Fatalln(http.ListenAndServe(":80", nil))
 }
